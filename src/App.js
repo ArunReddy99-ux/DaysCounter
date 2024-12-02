947dcd1ad3947183ce6499dc80d6a430fbe7135a
ArunReddy99-ux <arunreddyveerelly2003@gmail.com>
2024-12-02 15:49:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
+function Counter() {
+  const [slidervalue, setSlidervalue] = useState(1);
+  const [count, setCount] = useState(0);
 
-export default App;
+  function handlencount() {
+    setCount(count + slidervalue);
+  }
+  function handlepcount() {
+    setCount(count - slidervalue);
+  }
+  function handlereset() {
+    setCount(0);
+    setSlidervalue(1);
+  }
+  const date = new Date("October 30 2024");
+  date.setDate(date.getDate() + count);
+  return (
+    <div classsName="App">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={slidervalue}
+          onChange={(e) => setSlidervalue(Number(e.target.value))}
+        />
+        <span>{slidervalue}</span>
+      </div>
+      <div>
+        <button onClick={handlencount}>+</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={handlepcount}>-</button>
+        <p>
+          <span>
+            {count === 0
+              ? "Today is"
+              : count > 0
+              ? `${Math.abs(count)} days from today is `
+              : `${Math.abs(count)}days ago was`}
+          </span>
+          <span>{date.toDateString()}</span>
+        </p>
+      </div>
+      {count !== 0 || slidervalue !== 1 ? (
+        <div>
+          <button onClick={handlereset}>RESET</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
